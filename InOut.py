ad3c27920dc2f8c56122a2008d486282946ba618
orshemesh <chikos2011@gmail.com>
2020-06-06 12:47:32 +0300

    Commit with circle

--- InOut.py
+++ InOut.py
@@ -1,39 +1,31 @@
+# Python3 program to check if
+# a point lies inside a circle
+# or not
+import math
+def isInside(circle_x, circle_y, rad, x, y):
 
-# Python3 program to Check
-# if a point lies on or
-# inside a rectangle | Set-2
+	# Compare radius of circle
+	# with distance of its center
+	# from given point
+	if ((x - circle_x) * (x - circle_x) +
+		(y - circle_y) * (y - circle_y) <= rad * rad):
+		return True;
+	else:
+		return False;
 
-# function to find if
-# given point lies inside
-# a given rectangle or not.
-def FindPoint(x1, y1, x2,
-              y2, x, y) :
-    if (x >= x1 and x <= x2 and
-        y >= y1 and y <= y2) :
-        return True
-    else :
-        return False
-
-# Driver code
-if __name__ == "__main__" :
-
-    # bottom-left and top-right
-    # corners of rectangle.
-    # use multiple assigment
-    #(['96.3', '1', '70'], ['96.7', '1', '71'])
-    x1 , y1 , x2 , y2 = 96.3, 70, 96.7, 71
-
-    # given point
-    #['96.3', '1', '70'] in
-    #['97.0', '1', '80']) out
-    x, y =97.0, 80
-
-    # function call
-    if FindPoint(x1, y1, x2,
-                 y2, x, y) :
-        print("Yes")
-    else :
-        print("No")
+# Driver Code
+x = 1;
+y = 2;
+circle_x = 0;
+circle_y = 0;
+circle_x1 = 0;
+circle_y1 = 2;
+rad = math.sqrt((circle_x-circle_x1)**2+(circle_y-circle_y1)**2)
+print(rad)
+if(isInside(circle_x, circle_y, rad, x, y)):
+	print("Inside");
+else:
+	print("Outside");
 
 # This code is contributed
-# by Ankit Rai
+# by mits.
